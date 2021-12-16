f516371bd2f1c8daed63e7b1febe35be78606853
César Abril <cdgonzalezab@gmail.com>
2021-12-16 04:14:55 -0500

    router y main

--- README.md
+++
@@ -1,3 +0,0 @@
-# G5_P5_frontend
-
-En la carpeta "frontend prueba" se encuentran diferentes archivos html que necesitan ser modificados.
--- deployment.sh
+++
@@ -1,5 +0,0 @@
-sudo heroku login
-sudo heroku create misiontic-2021-frontend
-sudo heroku container:login
-sudo heroku container:push web --app misiontic-2021-frontend
-sudo heroku container:release web --app misiontic-2021-frontend
